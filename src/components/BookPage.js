b8086ed3512729561ddec65ba0791478b5217e49
SzWoz <85839038+SzWoz@users.noreply.github.com>
2022-05-23 23:20:07 +0200

    fetched book data

--- src/components/BookPage.js
+++ src/components/BookPage.js
@@ -1,6 +1,24 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 
 
 const BookPage = () => {
+
+    const { id } = useParams()
+    const [book, setBook] = useState()
+
+    useEffect(() => {
+        const getBookData = async () => {
+            const res = await fetch(`https://gnikdroy.pythonanywhere.com/api/book/${id}`);
+            const data = await res.json();
+
+            setBook(data)
+        }
+        getBookData();
+    }, [id])
+
+    console.log(book)
     return (
         <p>asd</p>
     )
